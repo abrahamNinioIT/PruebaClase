daab0017820715778cf1a0972a8fa6403cf77eae
DAW <DAW@GEST-PC-01>
2020-01-09 20:14:12 +0100

    pruba lo mismo

--- Prueba/src/ejercicios/Prueba1.java
+++ Prueba/src/ejercicios/Prueba1.java
@@ -5,6 +5,10 @@ public class Prueba1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Prueba cambio para GIT");
+		System.out.println("Prueba cambio para GIT");
+
+		System.out.println("Prueba cambio para GIT");
+
 	}
 
 }
