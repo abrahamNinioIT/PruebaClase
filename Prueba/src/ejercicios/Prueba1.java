7c8847f808f8dd3c60270fd8fe510f7e7f37e897
DAW <DAW@GEST-PC-01>
2020-01-09 20:11:14 +0100

    Prueba añaddido mensaje

---
+++ Prueba/src/ejercicios/Prueba1.java
@@ -0,0 +1,10 @@
+package ejercicios;
+
+public class Prueba1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Prueba cambio para GIT");
+	}
+
+}
