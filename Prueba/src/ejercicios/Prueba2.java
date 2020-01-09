6222ea9aa26c17f718b1efe6aa7e9cf3b106977c
DAW <DAW@GEST-PC-01>
2020-01-09 20:46:24 +0100

    Verison 1 con javadoc

--- Prueba/src/ejercicios/Prueba2.java
+++ Prueba/src/ejercicios/Prueba2.java
@@ -1,7 +1,11 @@
 package ejercicios;
-
+/**
+ * @author Abraham Nevado Rico
+ * @version 1.0
+ */
 public class Prueba2 {
 public static void main(String[] args) {
 	System.out.println("Otra pruebad e commuit");
+
 }
 }
