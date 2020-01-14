1dc227c6deb99de9e729e5f9e34694f2f7b67178
DAW <DAW@GEST-PC-01>
2020-01-14 16:07:03 +0100

    Otra prueba de comentario

--- Prueba/src/ejercicios/Prueba2.java
+++ Prueba/src/ejercicios/Prueba2.java
@@ -7,5 +7,6 @@ public class Prueba2 {
 public static void main(String[] args) {
 	System.out.println("Otra pruebad e commuit");
 	//Modificacion para hacer un branch
+	//Otra modificacion de prueba
 }
 }
